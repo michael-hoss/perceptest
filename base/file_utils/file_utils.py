78b0e6d30c9528ea1f36df8ff453caa471e7316a
Michael Hoss <michael.hoss@spleenlab.ai>
2024-10-18 12:56:10 +0200

    beautify code

--- base/file_utils/file_utils.py
+++ base/file_utils/file_utils.py
@@ -1,4 +1,6 @@
 import hashlib
+import os
+import shutil
 import zipfile
 
 
@@ -16,3 +18,17 @@ def unzip_file(zip_file_path: str, extract_to_dir: str):
             zip_ref.extractall(extract_to_dir)
     else:
         raise Exception(f"{zip_file_path} is not a valid zip file")
+
+
+def silent_remove_file(file: str) -> None:
+    try:
+        os.unlink(file)
+    except FileNotFoundError:
+        pass
+
+
+def silent_remove_dir(dir: str) -> None:
+    try:
+        shutil.rmtree(dir)  # Remove directory and its contents
+    except FileNotFoundError:
+        pass
--- inputs/artery/conftest.py
+++ inputs/artery/conftest.py
@@ -1,12 +1,15 @@
 import os
-import shutil
 
 import pytest
 import requests  # type: ignore
 
-from base.file_utils.file_utils import compute_file_hash, unzip_file
+from base.file_utils.file_utils import compute_file_hash, silent_remove_dir, silent_remove_file, unzip_file
 from inputs.artery.artery_format import ArterySimLog, ArterySimLogDump
 from inputs.artery.from_logs.raw_parsing import load_from_artery_logs
+
+# Keep those up-to-date!
+TEST_ZIP_DOWNLOAD_URL = "https://rwth-aachen.sciebo.de/s/X8WzHWqTwUsEsUz/download"
+TEST_ZIP_HASH = "141eaef417ea1843bfafb30218c17e82a35d79f077051283d529550a5c3a27f9"
 
 
 def get_artery_data_root() -> str:
@@ -17,47 +20,53 @@ def get_artery_data_root() -> str:
     return artery_data_root
 
 
-def get_test_cases_zip() -> str:
-    data_root = get_artery_data_root()
-    zip_file_path = os.path.join(data_root, "artery_data.zip")
+def get_test_cases_zip(zip_file_path: str, unzipped_dir: str) -> None:
+    if not correct_zip_file_exists(zip_file_path=zip_file_path):
+        silent_remove_file(file=zip_file_path)
+        silent_remove_dir(dir=unzipped_dir)
+        download_zip_file(zip_file_path=zip_file_path)
 
+
+def correct_zip_file_exists(zip_file_path: str) -> bool:
     if os.path.isfile(zip_file_path):
-        expected_zip_hash = "1b56035be1e4864e0ae678282336318c52c459c83ed56f82ae7861215e4c6662"
         actual_zip_hash = compute_file_hash(file_path=zip_file_path)
-        if actual_zip_hash == expected_zip_hash:
-            return zip_file_path
-        else:
-            unzipped_dir = os.path.join(data_root, "example-artery-data")
-            os.unlink(zip_file_path)
+        if actual_zip_hash == TEST_ZIP_HASH:
+            return True
+    return False
 
-    shutil.rmtree(unzipped_dir)  # Remove directory and its contents
 
-    artery_url = "https://rwth-aachen.sciebo.de/s/X8WzHWqTwUsEsUz/download"
-    response = requests.get(artery_url)
+def download_zip_file(zip_file_path: str) -> None:
+    response = requests.get(TEST_ZIP_DOWNLOAD_URL)
     response.raise_for_status()  # Ensure the request was successful
 
     with open(zip_file_path, "wb") as f:
         f.write(response.content)
-    return zip_file_path
+
+    assert compute_file_hash(file_path=zip_file_path) == TEST_ZIP_HASH
 
 
-def get_test_cases_dir_from_zip(zip_file_path: str):
-    extract_to_dir: str = os.path.dirname(zip_file_path)
-    unzipped_dir = os.path.join(extract_to_dir, "example-artery-data")
+def get_test_cases_dir_from_zip(zip_file_path: str, unzipped_dir: str) -> None:
+    extract_to_dir: str = os.path.dirname(unzipped_dir)
     if not os.path.isdir(unzipped_dir):
         unzip_file(zip_file_path=zip_file_path, extract_to_dir=extract_to_dir)
-    return unzipped_dir
+    else:
+        # We assume the data is already correctly unzipped froma previous execution
+        pass
 
 
 def get_test_case_dump() -> ArterySimLogDump:
     """Enable calling this directly for manual tests (analysis files) that are not pytest tests,
     but rather just bazel py_binary files for easier debugging."""
 
-    zip_file_path: str = get_test_cases_zip()
-    unzipped_dir: str = get_test_cases_dir_from_zip(zip_file_path=zip_file_path)
+    data_root = get_artery_data_root()
+    zip_file_path = os.path.join(data_root, "artery_data.zip")
+    unzipped_dir = os.path.join(data_root, "example-artery-data")
+
+    get_test_cases_zip(zip_file_path=zip_file_path, unzipped_dir=unzipped_dir)
+    get_test_cases_dir_from_zip(zip_file_path=zip_file_path, unzipped_dir=unzipped_dir)
 
     dump_v6_sim15_01 = ArterySimLogDump(
-        root_dir=os.path.join(unzipped_dir, "v6/config_medium/full_run_01"),
+        root_dir=os.path.join(unzipped_dir, "v6/sim15data/results_01"),
         res_file="localperceptionGT-vehicle_0.out",
         out_file="localperception-vehicle_0.out",
         ego_file="monitor_car-vehicle_0.out",
